5afb0f84bad89fec747cbff5f617f7a719f44dd4
paulemileatn <paulemileatn@gmail.com>
2025-03-23 20:10:22 +0100

    You only need one - Beginner

--- 8-kyu/script.js
+++ 8-kyu/script.js
@@ -5,3 +5,6 @@ const betterThanAverage = (classPoints, yourPoints) => {
     allPoints.reduce((acc, score) => acc + score, 0) / allPoints.length;
   return yourPoints > classAverage;
 };
+
+// You only need one - Beginner
+const check = (a, x) => a.includes(x);

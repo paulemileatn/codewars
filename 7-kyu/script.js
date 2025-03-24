10d7788765998a7e144cfc40c257e5a00a23795d
paulemileatn <paulemileatn@gmail.com>
2025-03-24 15:50:32 +0100

    Evens times last

--- 7-kyu/script.js
+++ 7-kyu/script.js
@@ -5,3 +5,12 @@ const longestWord = stringOfWords => {
     .sort((a, b) => a.length - b.length)
     .at(-1);
 };
+
+// Evens times last
+const evenLast = numbers =>
+  numbers.length
+    ? numbers.reduce(
+        (acc, number, index) => (index % 2 === 0 ? acc + number : acc),
+        0
+      ) * numbers.at(-1)
+    : 0;
